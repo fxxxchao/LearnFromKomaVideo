3448c0fc1b1d334d84d15e180b5c5a23db4a1cd2
Chao <fxxxchao@gmail.com>
2021-11-19 12:03:08 +0800

    fix

--- Lession 02 - ImageZoom/ImageZoom/ViewController.swift
+++ Lession 02 - ImageZoom/ImageZoom/ViewController.swift
@@ -6,13 +6,40 @@
 //
 
 import UIKit
+import SwiftUI
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
     }
+    
+    struct ContentView: View {
+        
+        @state private var zoomd :Bool = false
+        
+        var body: some View {
+            ZStack(alignment: self.zoomd ? .top: .topTrailing) {
+                Image("")
+                    .resizable()
+                    .aspectRatio(contentMode: self.zoomd ? .fill:.fit)
+                    .navigationTitle(<#T##title: Text##Text#>)
+            }
+        }
+    }
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            NavigationView {
+                ContentView();
+            }
+        }
+    }
+    
+    
 
 
 }
