9ccb7c37b45a5826af746ca4b70a8e2a90eab853
Chao <fxxxchao@gmail.com>
2022-08-09 11:14:18 +0800

    更新

--- Lession 02 - ImageZoom/ImageZoom/ViewController.swift
+++ Lession 02 - ImageZoom/ImageZoom/ViewController.swift
@@ -5,8 +5,48 @@
 //  Created by 房超 on 2021/11/18.
 //
 
-import UIKit
 import SwiftUI
+
+struct ContentView: View {
+    
+    private var zoomd :Bool = false
+    
+    var body: some View {
+        ZStack(alignment: self.zoomd ? .top: .topTrailing) {
+            
+            Image("abc")
+                .resizable()
+                .aspectRatio(contentMode: self.zoomd ? .fill:.fit)
+                .navigationBarTitle(Text("ABCD"),displayMode:.inline)
+                .onTapGesture {
+                    withAnimation {
+                        
+                    }
+                }
+        
+            Image("123")
+                .resizable()
+                .frame(width: 50, height: 50, alignment: .center)
+                .padding(.all, 10)
+                .navigationTitle("哈哈")
+            
+            Image("123")
+                .resizable()
+                .frame(width: 30, height: 30, alignment: .bottom)
+                .padding(.all,20)
+                .navigationTitle("CCCC")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView {
+            ContentView()
+        }
+    }
+}
+
 
 class ViewController: UIViewController {
 
@@ -14,33 +54,8 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        
+    
     }
-    
-    struct ContentView: View {
-        
-        @state private var zoomd :Bool = false
-        
-        var body: some View {
-            ZStack(alignment: self.zoomd ? .top: .topTrailing) {
-                Image("")
-                    .resizable()
-                    .aspectRatio(contentMode: self.zoomd ? .fill:.fit)
-                    .navigationTitle(<#T##title: Text##Text#>)
-            }
-        }
-    }
-    
-    struct ContentView_Previews: PreviewProvider {
-        static var previews: some View {
-            NavigationView {
-                ContentView();
-            }
-        }
-    }
-    
-    
-
 
 }
 
